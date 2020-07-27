75d1b7257c00679aa1f96812622c38bb73bfec18
your name <you@example.com>
2020-07-27 18:16:00 +0800

    2020-07-25 18:14

--- back-end/service/service_acl/src/main/java/com/atguigu/aclservice/config/MybatisPlusMetaObjectHandler.java
+++ back-end/service/service_acl/src/main/java/com/atguigu/aclservice/config/MybatisPlusMetaObjectHandler.java
@@ -14,14 +14,14 @@ public class MybatisPlusMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         this.setFieldValByName("gmtCreate", new Date(), metaObject);
-        this.setFieldValByName("gmtUpdate", new Date(), metaObject);
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
 //        this.setFieldValByName("version", Long.valueOf(1), metaObject);
 //        this.setFieldValByName("appSecret", UUID.randomUUID().toString().replaceAll("-", ""), metaObject);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        this.setFieldValByName("gmtUpdate", new Date(), metaObject);
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
 //        this.setFieldValByName("version", this.getFieldValByName("version", metaObject), metaObject);
     }
 }
